e5d1624361a2d0b0c1b43b4c5a693073033ab7d4
krishna jadhav <krishnajadhav0807@gmail.com>
2025-03-21 23:51:13 +0530

    initial commit

---
+++ autopark/src/test/java/com/autopark/autopark/AutoparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autopark.autopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
